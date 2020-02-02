1f17852d5916216af8e6ed02594f78ff5bd5824f
Elen-pollyc137 <poliana_elen@hotmail.com>
2020-02-02 08:11:25 -0300

    filter explicacao

--- Array/filter1.js
+++ Array/filter1.js
@@ -1,8 +1,8 @@
 const produto = [
 {nome:'Notebook', preco:2400, fragil: true},
-{nome:'Notebook', preco:2400, fragil: true},
-{nome:'Notebook', preco:2400, fragil: false},
-{nome:'Notebook', preco:2400, fragil: true}
+{nome:'iPad Pro', preco:4199, fragil: true},
+{nome:'Copo de Vidro', preco:12.49, fragil: true},
+{nome:'Copo de Plastico', preco:18.99, fragil: false}
 ]
 console.log(produto.filter(function(p){
     return p.preco
