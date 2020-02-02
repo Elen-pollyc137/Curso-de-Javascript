86b1883b77a538dc3dccffadcf67a351dc0ecf3b
Elen-pollyc137 <poliana_elen@hotmail.com>
2020-02-02 08:41:36 -0300

    reduce

--- Array/reduce2.js
+++ Array/reduce2.js
@@ -4,6 +4,9 @@ const alunos = [
     { nome: 'Maria', nota: 9.2, bolsista: true },
     { nome: 'Pedro', nota: 9.8, bolsista: false },
     { nome: 'Ana', nota: 8.7, bolsista: true }
-]
+];
 const todosBolsistas = (resultado, bolsista) => resultado && bolsista
-console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas));
+
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista));
