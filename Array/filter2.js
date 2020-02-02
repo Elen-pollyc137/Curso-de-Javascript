1967e0fcfc1aace65ea3511992102148afde6acf
Elen-pollyc137 <poliana_elen@hotmail.com>
2020-02-02 08:28:34 -0300

    filter metodo prototype

--- Array/filter2.js
+++ Array/filter2.js
@@ -1,11 +1,11 @@
 Array.prototype.filter2 = function(callback) {
-    const newArray = []
+    const newArray = []//os elementos filtrados representa um novo array
     for (let i = 0; i < this.length; i++) {
-        if(callback(this[i], i, this)) {
-            newArray.push(this[i])
+        if(callback(this[i], i, this)); {
+            newArray.push(this[i]);
         }
     }
-    return newArray
+    return newArray;
 }
 
 const produtos = [
@@ -13,10 +13,10 @@ const produtos = [
     { nome: 'iPad Pro', preco: 4199, fragil: true },
     { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
     { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
-]
+];
 
-const caro = produto => produto.preco >= 500
-const fragil = produto => produto.fragil
+const caro = produto => produto.preco >= 500;
+const fragil = produto => produto.fragil;
 
-console.log(produtos.filter2(caro).filter2(fragil))
+console.log(produtos.filter2(caro).filter2(fragil));
 //exercicio concluido
