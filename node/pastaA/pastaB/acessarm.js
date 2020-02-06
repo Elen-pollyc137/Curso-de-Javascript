3e6adbd59b4585e91a8c9274d72af5ede5d0e7d1
Elen-pollyc137 <poliana_elen@hotmail.com>
2020-02-06 14:06:10 -0300

    node

--- node/pastaA/pastaB/acessarm.js
+++ node/pastaA/pastaB/acessarm.js
@@ -1,3 +1,13 @@
 const moduloA = require('../../modulo1')
 console.log(moduloA.ateLogo)
 //esse exemplo so funciona no mac e windows
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+/*const http = require('http')
+http.createServer((req, res) => {
+    res.end()
+}).listen(8080)*/
