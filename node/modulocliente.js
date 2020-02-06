8b105359cf1d45edfa97ec2d7c4f88249f76f5bb
Elen-pollyc137 <poliana_elen@hotmail.com>
2020-02-06 08:13:52 -0300

    node

--- node/modulocliente.js
+++ node/modulocliente.js
@@ -1,10 +1,6 @@
-const modulo1 = require('./mudulo1');
-
-const modulo2 = require('./module2');
+const modulo1 = require('./modulo1')
 
 console.log(modulo1.ola)
 console.log(modulo1.bemVindo)
 console.log(modulo1.ateLogo)
 
-console.log(modulo2.Bomdia)
-console.log(modulo2.boanoite())
